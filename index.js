ada2aacba2e6ecd2616234d0f674a06822908c8b
asaad8756 <69296518+asaad8756@users.noreply.github.com>
2020-10-26 09:42:09 +0100

    26/10/20

--- index.js
+++ index.js
@@ -3,3 +3,10 @@
 This is a 
 multiline comment
  */
+document.getElementById('bt').addEventListener('click', function() {
+    alert('Hello Vanilla Js');
+});
+
+document.getElementById('bt').addEventListener('click', function(){
+    alert('Hey, this work');
+});
